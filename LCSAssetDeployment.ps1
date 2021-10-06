e55d2d2fcc848a9fb6dbbfb08773307daf9080df
ciellos-dev <67259746+ciellos-dev@users.noreply.github.com>
2021-10-06 14:01:33 +0300

    Create LCSAssetDeployment.ps1

---
+++ LCSAssetDeployment.ps1
@@ -0,0 +1,17 @@
+    [CmdletBinding()]
+    param(
+        [Parameter()]
+        [string]$ClientId,
+        [string]$Username,
+        [string]$Password,
+        [string]$FilePath,
+        $ProjectId,
+        [string]$LCSEnvironmentID,
+        [string]$LCSFileAssetID,
+        [string]$UpdateName)
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Install-PackageProvider nuget -Scope CurrentUser -Force -Confirm:$false
+Install-Module -Name AZ -AllowClobber -Scope CurrentUser -Force -Confirm:$False -SkipPublisherCheck
+Install-Module -Name d365fo.tools -AllowClobber -Scope CurrentUser -Force -Confirm:$false
+Get-D365LcsApiToken -ClientId $ClientId -Username $Username -Password $Password -LcsApiUri "https://lcsapi.lcs.dynamics.com" -Verbose | Set-D365LcsApiConfig -ProjectId $ProjectId
+Invoke-D365LcsDeployment -AssetId $LCSFileAssetID -EnvironmentId $LCSEnvironmentID -UpdateName $UpdateName
