5c8919c95b05d6449a022bf6bab4529110e0d961
ciellos-dev <67259746+ciellos-dev@users.noreply.github.com>
2021-10-11 08:44:41 +0300

    Update LCSAssetDeployment.ps1

--- LCSAssetDeployment.ps1
+++ LCSAssetDeployment.ps1
@@ -23,6 +23,10 @@ if( -not ((Find-Module -Name d365fo.tools).Name -eq 'd365fo.tools'))
 {
     Install-Module -Name d365fo.tools -AllowClobber -Scope CurrentUser -Force -Confirm:$false
 }
+else
+{
+     Import-Module -Name d365fo.tools -AllowClobber -Scope CurrentUser -Force -Confirm:$false
+}
 Get-D365LcsApiToken -ClientId $ClientId -Username $Username -Password $Password -LcsApiUri "https://lcsapi.lcs.dynamics.com" -Verbose | Set-D365LcsApiConfig -ProjectId $ProjectId
 $PSFObject = Invoke-D365LcsDeployment -AssetId $LCSFileAssetID -EnvironmentId $LCSEnvironmentID -UpdateName $UpdateName
 Get-D365LcsDeploymentStatus -ActivityId $PSFObject.ActivityId -EnvironmentId $LCSEnvironmentID -WaitForCompletion
