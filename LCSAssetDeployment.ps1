12c55388c676f43571e95c2a1185444c983be114
ciellos-dev <67259746+ciellos-dev@users.noreply.github.com>
2021-10-20 07:53:25 +0300

    Update LCSAssetDeployment.ps1

--- LCSAssetDeployment.ps1
+++ LCSAssetDeployment.ps1
@@ -11,9 +11,22 @@
         [string]$UpdateName)
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 #Check Modules installed
-Install-PackageProvider nuget -Scope CurrentUser -Force -Confirm:$false
-Install-Module -Name AZ -AllowClobber -Scope CurrentUser -Force -Confirm:$False -SkipPublisherCheck
-Install-Module -Name d365fo.tools -AllowClobber -Scope CurrentUser -Force -Confirm:$false
+$NuGet = Get-PackageProvider -Name nuget -ErrorAction SilentlyContinue
+$Az = Get-InstalledModule -Name AZ -ErrorAction SilentlyContinue
+$DfoTools = Get-InstalledModule -Name d365fo.tools -ErrorAction SilentlyContinue
+
+if([string]::IsNullOrEmpty($NuGet))
+{
+    Install-PackageProvider nuget -Scope CurrentUser -Force -Confirm:$false
+}
+if([string]::IsNullOrEmpty($Az))
+{
+    Install-Module -Name AZ -AllowClobber -Scope CurrentUser -Force -Confirm:$False -SkipPublisherCheck
+}
+if([string]::IsNullOrEmpty($DfoTools))
+{
+    Install-Module -Name d365fo.tools -AllowClobber -Scope CurrentUser -Force -Confirm:$false
+}
 
 Get-D365LcsApiToken -ClientId $ClientId -Username $Username -Password $Password -LcsApiUri "https://lcsapi.lcs.dynamics.com" -Verbose | Set-D365LcsApiConfig -ProjectId $ProjectId
 $PSFObject = Invoke-D365LcsDeployment -AssetId $LCSFileAssetID -EnvironmentId $LCSEnvironmentID -UpdateName $UpdateName -Verbose
